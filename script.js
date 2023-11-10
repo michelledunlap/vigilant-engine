67b500ab8c285c7782e04a3fe4100160becd68f8
Michelle Dunlap <49043261+michelledunlap@users.noreply.github.com>
2023-11-10 12:26:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('.emotion').forEach(emotion => {
         emotion.addEventListener('click', function() {
-            const relatedSection = document.getElementById(this.dataset.related);
-            if (relatedSection) {
-                // Toggle the related section visibility
-                relatedSection.style.display = relatedSection.style.display === 'none' ? 'block' : 'none';
-            }
             // Toggle greyed-out class
             this.classList.toggle('greyed-out');
-            if (this.dataset.related) {
-                // Grey out all related emotions
-                document.querySelectorAll('#' + this.dataset.related + ' .emotion').forEach(relatedEmotion => {
-                    relatedEmotion.classList.add('greyed-out');
+            // If this is a sub-emotion, grey out its children
+            let nextElement = this.nextElementSibling;
+            while(nextElement && nextElement.classList.contains('emotion-subcolumn')) {
+                Array.from(nextElement.querySelectorAll('.emotion')).forEach(child => {
+                    child.classList.add('greyed-out');
                 });
+                nextElement = nextElement.nextElementSibling;
             }
         });
     });
